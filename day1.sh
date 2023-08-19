d5c4681a10c7445f132e4fc71c93b92a864d2fbe
Pokala Jayanth <Pokala.Jayanth@tatacommunications.com>
2023-08-20 00:46:26 +0530

    update check

--- day1.sh
+++ day1.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 USER1=SURESH
 USER2=RAMESH
+DATE=$(date)
 echo "this is day1, i need fiish by tomorrow"
 echo "${USER1}, CAN U WORK TODAY ?"
 echo "$USER2 AND $USER1 are idiots "
+echo " today date is $DATE"
