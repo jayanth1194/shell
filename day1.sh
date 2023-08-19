ecf50d76c6e760ef4245d55afa2888a85360ea54
Pokala Jayanth <Pokala.Jayanth@tatacommunications.com>
2023-08-20 01:11:03 +0530

    update check

--- day1.sh
+++ day1.sh
@@ -1,8 +1,5 @@
 #!/bin/bash 
 USER1=$1
 USER2=$2
-DATE=$(date)
-echo "this is day1, i need fiish by tomorrow"
-echo "${USER1}, CAN U WORK TODAY ?"
-echo "$USER2 AND $USER1 are idiots "
-echo " today date is $DATE"
+SUM=$(USER1+USER2)
+echo "$SUM"
