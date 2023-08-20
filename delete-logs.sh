42fc69644c267e7ee584e76dfa777abb789265d5
Pokala Jayanth <Pokala.Jayanth@tatacommunications.com>
2023-08-21 01:47:04 +0530

    ..

--- delete-logs.sh
+++ delete-logs.sh
@@ -6,7 +6,8 @@ SCRIPT_NAME=$0
 SCRIPT_FILE=/tmp/$SCRIPT_NAME-$DATE.log
 
 OLD_FILES=$(find $LOG_FILE -name "*.log"  -type f -mtime +14)
-NUMBER_OLD_FILES=$(cat $LOG_FILE |wc-l)
+NUMBER_OLD_FILES=$(cat $LOG_FILE |wc -l)
+echo "$OLD_FILES,$NUMBER_OLD_FILES "
 
 
 
