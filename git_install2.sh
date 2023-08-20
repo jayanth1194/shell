1bddaf8b4d81a686c84c31f3c5b1d58c55efb2b9
Pokala Jayanth <Pokala.Jayanth@tatacommunications.com>
2023-08-20 18:22:58 +0530

    ..

--- git_install2.sh
+++ git_install2.sh
@@ -9,15 +9,15 @@ then
     echo " need sudo access"
     exit 1
 fi 
-yum install nginxss -y 
+# yum install nginxss -y 
 
-if [ $? -ne 0 ]
-then 
-    echo " exit status is not zero "
-    exit 2
-else 
-    echo " install of nginx is success"
-fi 
+# if [ $? -ne 0 ]
+# then 
+#     echo " exit status is not zero "
+#     exit 2
+# else 
+#     echo " install of nginx is success"
+# fi 
 
 yum install postfix -y 
 
