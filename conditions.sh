b2ce1e3acc5a22ec1a1558f769f52c88d65458c4
Pokala Jayanth <Pokala.Jayanth@tatacommunications.com>
2023-08-20 17:00:08 +0530

    conditions updated

--- conditions.sh
+++ conditions.sh
@@ -1,10 +1,14 @@
 #!/bin/bash 
 
- echo " enter the number :"
- read -s NUMBER 
 
- if [ $NUMBER -gt 10 ] then 
- echo "num greater than 10"
- else 
- echo " Num less than 10 "
- fi 
+
+echo "Enter the number here :" 
+read -s NUMBER
+
+if [ $NUMBER -gt 10]
+then
+    echo " number is greater than 10"
+else 
+    echo " number is less than 10 "
+fi
+
