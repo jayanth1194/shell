36fd2505bec3b55f6ebc88ec2a4cbf2bdb5f02b0
Pokala Jayanth <Pokala.Jayanth@tatacommunications.com>
2023-08-21 17:28:36 +0530

    delete logs

--- delete-logs.sh
+++ delete-logs.sh
@@ -9,8 +9,10 @@ OLD_FILES=$(find $LOG_FILE -name "*.log"  -type f -mtime +14)
 NUMBER_OLD_FILES=$(cat $LOG_FILE |wc -l)
 echo "$OLD_FILES,$NUMBER_OLD_FILES "
 
-
-
-
+while read line
+do 
+    echo " deleteing the type  $line"
+    rm -rf $line
+done <<< $OLD_FILES
 
 
