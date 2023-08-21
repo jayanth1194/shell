5881f03b9d774a8484bcb236e699c4f9c566d3e1
Pokala Jayanth <Pokala.Jayanth@tatacommunications.com>
2023-08-21 18:37:59 +0530

    2 min delete logs

--- delete-logs.sh
+++ delete-logs.sh
@@ -8,7 +8,7 @@ SCRIPT_FILE=/tmp/$SCRIPT_NAME-$DATE.log
 OLD_FILES=$(find $LOG_FILE -name "*.log"  -type f  -mmin +2 )
 NUMBER_OLD_FILES=$(cat $LOG_FILE | wc -l)
 echo "$OLD_FILES,$NUMBER_OLD_FILES "
-echo "script started executing at $DATE" &>>SCRIPT_FILE
+echo "script started executing at $DATE" &>> $SCRIPT_FILE
 while read line
 do 
     echo " deleteing the type  $line"
