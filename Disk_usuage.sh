64fb915da31f86301b007605acef499d82855bc6
Pokala Jayanth <Pokala.Jayanth@tatacommunications.com>
2023-08-22 22:38:27 +0530

    disk

--- Disk_usuage.sh
+++ Disk_usuage.sh
@@ -19,11 +19,12 @@ do
     TYPE=$(echo $line | awk -F ' ' '{print $1}')
     if [ $USUAGE -gt  12 ]
     then 
-    message="HIGH DISK USUAGE OF $TYPE : $USUAGE"
-    echo "$message"
+    message+="HIGH DISK USUAGE OF $TYPE : $USUAGE"
+    
     fi 
 
 done <<< $DISK_STORGE
 
+echo "$message "|mail -s "DISK " jayantjae@gmail.com
 
 
