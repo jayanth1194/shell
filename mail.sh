ff6730ffa6633183ced6dae3df59bcbd81bd9de1
Pokala Jayanth <Pokala.Jayanth@tatacommunications.com>
2023-08-23 01:04:08 +0530

    disk

--- mail.sh
+++ mail.sh
@@ -3,10 +3,10 @@
 
 TO_ADDRESS=$1
 SUBJECT=$2
-BODY=$3
+BODY=$(sed -e 's/[]\/$*.^[]/\\&/g' <<< $3) 
 ALERT_TYPE=$4
 NAME=$5
 #mail.sh toaddreess, subject, body alert-type name 
 
-BODY1=$(sed -e 's/Team/QA TEAM' -e 's/alertype/high disk' -e "s/MESSAGE/$BODY" template.html)
+BODY1=$(sed -e "s/Team/$NAME" -e "s/alertype/$ALERT_TYPE" -e "s/MESSAGE/$BODY" template.html)
 echo "$BODY1"| mail -s "$SUBJECT" $TO_ADDRESS
