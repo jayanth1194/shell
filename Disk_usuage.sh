a859b98acf4e810f3225c2fbbdfd79016364fad7
Pokala Jayanth <Pokala.Jayanth@tatacommunications.com>
2023-08-22 18:47:18 +0530

    disk usuage

---
+++ Disk_usuage.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+DATE=$(date +%F)
+
+LOG_FILE=/tmp
+
+DISK_STORGE=$(df -hT | grep -vE 'tmpfs|File')
+
+DISK_THRESOLD=12
+
+
+while read line
+
+do 
+
+    USUAGE=$(echo $line | awk -F ' ' '{print $1}')
+    TYPE=$(echo $line | awk -F ' ' '{print $2}')
+    if [ $USUAGE -gt  12 ]
+    then 
+    echo 'HIGH DISK USUAGE OF $TYPE : $USUAGE'
+    fi 
+
+done
+
