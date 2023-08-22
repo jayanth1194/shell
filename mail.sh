2d45c1a9ca93ebce8c9660a547d455a7f15ae7ac
Pokala Jayanth <Pokala.Jayanth@tatacommunications.com>
2023-08-23 01:17:09 +0530

    disk

--- mail.sh
+++ mail.sh
@@ -9,4 +9,4 @@ NAME=$5
 #mail.sh toaddreess, subject, body alert-type name 
 
 BODY1=$(sed -e "s/Team/$NAME/g" -e "s/alertype/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/g" template.html)
-echo "$BODY1"| mail -s "$SUBJECT" $TO_ADDRESS
+echo "$BODY1"| mail -s "$SUBJECT" "$TO_ADDRESS"
